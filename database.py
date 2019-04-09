2a2482136aa2473ef672979fe98934498596fdd4
khengen <khengen@wustl.edu>
2019-04-09 18:18:32 -0500

    adding the first mySQL database code. this is for adding data to existing db.

---
+++ database.py
@@ -0,0 +1,93 @@
+''' This function asks a user to input the implant/region info about each electrode used in chronic electrophys recordings. The function will write a csv file into the folder that contains the relevant dataset. The csv file will be uploaded into the lab's mysql database into the implant_db table in the clusters database.'''
+from tkinter import filedialog
+from tkinter import Tk
+from tkinter import messagebox
+import os
+import os.path as op
+import shutil
+import csv
+import mysql.connector as my
+
+# connect to the clustering database
+rootpwd = "%6m5kq2FymMXy5t3"
+
+db = my.connect(
+    host        = "localhost",
+    user        = "root",
+    passwd      = rootpwd,
+    database    = "clusteringdb"
+)
+cursor = db.cursor()
+
+# Tkinter
+root = Tk()
+root.withdraw()
+
+# Change to output directory
+chances_to_give = 3
+i = 0
+while i < chances_to_give + 1:
+    i = i + 1
+    dir_phy = filedialog.askdirectory( title = "Select output directory")
+    yes_no = messagebox.askyesno( title = "Are You Sure?",
+                                 message = Output directory : '+str(dir_phy) )
+    if yes_no:
+        print( "Current working directory is ", dir_phy )
+        #os.chdir(dir_phy)
+        break
+    else:
+        if i == chances_to_give:
+            raise Exception( 'Error: output directory not selected' )
+
+
+# these are the fields that the user needs to manually enter prior to uploading anything into the database.
+# implant (primary key) # THIS IS THE PRIMARY KEY AND WILL AUTOMATICALLY POPULATE WITH A UNIQUE VALUE
+animal_id           = input ('initials plus 5 digits, ex: abc12345')
+species             = input ('rat or mouse')
+sex                 = input ('m or f')
+region              = input ('common lab abbreviation, e.g., v1, v1m, scn, rsc, s1')
+strain              = input ('ex. c57b6, le (long evans), sd (sprague dawley)')
+genotype            = input ('ex. shank3+- or shank3 --')
+daq_system          = input ('ecube or intan')
+n_channels          = input ('total count in this implant, e.g., 64')
+n_sites             = input ('total number of sites on this animal')
+implant_date        = input ('(MMDDYYYY)')
+expt_start          = input ('(MMDDYYYY)')
+expt_end            = input ('(MMDDYYYY)')
+age_t0              = input ('(postnatal day e.g., 60)')
+surgeon             = input ('(3 letter initial, e.g., abc)')
+video_binary        = input ('yes video = 1, no video = 0')
+light_binary        = input ('L/D timestamps, 1 or 0')
+sound_binary        = input ('microphone data, 1 or 0')
+sw_binary           = input ('sleep scoring? 1 or 0')
+implant_coordinates = input ('stereotaxic coordinates (lateral, then bregma), e.g., -1.0, 2.2')
+electrode_type      = input ('e.g., mit silicon, carbon, tetrode')
+headstage           = input ('e.g., hs640, hs64, intan32, intan64')
+
+# write in a check for this. if the user wants to, s/he should be able to go back and edit before committing the info to the database.
+
+
+# upload this into the database:
+#  - - - - - - - - - - - - add data to a table - - - - - - - - - - - - -
+## defining the Query
+query = "INSERT INTO implant_db (animal_id, species, sex, region, strain, genotype, daq_system, n_channels, n_sites, implant_date, expt_start, expt_end, age_t0, surgeon, video_binary, light_binary, sound_binary, sw_binary, implant_coordinates, electrode_type, headstage ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+## storing values in a variable
+values = (animal_id, species, sex, region, strain, genotype, daq_system, n_channels, n_sites, implant_date, expt_start, expt_end, age_t0, surgeon, video_binary, light_binary, sound_binary, sw_binary, implant_coordinates, electrode_type, headstage)
+
+## executing the query with values
+cursor.execute(query, values)
+id = cursor.lastrowid()
+
+
+csvData = [ ['animal_id', animal_id],['species', species], ['sex', sex], ['region', region], ['strain', strain], ['genotype', genotype], ['daq_system', daq_system], ['n_channels', n_channels], ['n_sites', n_sites], ['implant_date', implant_date], ['expt_start', expt_start], ['expt_end', expt_end], ['age_t0', age_t0], ['surgeon', surgeon], ['video_binary', video_binary], ['light_binary', light_binary], ['sound_binary', sound_binary], ['sw_binary', sw_binary], ['implant_coordinates', implant_coordinates], ['electrode_type', electrode_type], ['headstage', headstage] ]
+
+
+
+#- - - - - - - - - - - - - - - - - insert data into table - - - - - - - - - - -
+sql_insert_query = " INSERT INTO `clusters`(`id`, `name`, `birth_date`, `age`) VALUES (1,'Scott','2018-04-11', 26) "
+
+with open('person.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(csvData)
+
+csvFile.close()
