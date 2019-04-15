a015e28176cf3cbaa96d8a34e1c0a0f94c30d152
kbhengen <khengen@wustl.edu>
2019-04-15 16:06:11 -0500

    more changes

--- database.py
+++ database.py
@@ -16,7 +16,6 @@ db = pymysql.connect(user   = "root",
 # )
 cursor = db.cursor()
 
-
 # - - - - - - - - - - -  delete table - - - - - - - - - - - - - - - - -
 def deltable(tablestring, cursor = cursor):
     '''Delete tables from the clusteringdb.
@@ -66,79 +65,68 @@ def createclusterstable():
     cursor.execute("ALTER TABLE clusters ADD FOREIGN KEY(implant_id) REFERENCES implant_db(implant_id) ")
     print('Created table "clusters" in the {} database'.format(db.db))
 
-def submit_implant():
+
+def __implantgui():
     ''' This function asks a user to input the implant/region info about each electrode used in chronic electrophys recordings. The function will write a csv file into the folder that contains the relevant dataset. The csv file will be uploaded into the lab's mysql database into the implant_db table in the clusters database.'''
-    # from tkinter import filedialog
-    # from tkinter import Tk
-    # from tkinter import messagebox
     import os
-    import os.path as op
-    import shutil
-    import csv
-    import pandas as pd
-
-
-    from __future__ import absolute_import
-    from traits.api import HasTraits, Str, Int, Enum, List, Range, Directory, Button
-    from traitsui.api import View, Item, Group, Handler, EnumEditor
+    from traits.api import HasTraits, Str, Enum, Range, Directory
+    from traitsui.api import View, Item, Handler  #Group,  #EnumEditor
     import numpy as np
 
-    # # Tkinter
-    # root = Tk()
-    # root.withdraw()
-    #
-    # # Change to output directory
-    # chances_to_give = 3
-    # i = 0
-    # while i < chances_to_give + 1:
-    #     i = i + 1
-    #     dir_phy = filedialog.askdirectory( title = "Select output directory")
-    #     yes_no = messagebox.askyesno( title = "Are You Sure?",
-    #                                  message = 'Output directory : '+str(dir_phy) )
-    #     if yes_no:
-    #         print( "Current working directory is ", dir_phy )
-    #         #os.chdir(dir_phy)
-    #         break
-    #     else:
-    #         if i == chances_to_give:
-    #             raise Exception( 'Error: output directory not selected' )
-    # Dictionaries of defined elements.
+    # set up dictionaries for GUI field enumeration
     specieslist = {
 
         'Unknown'   : ['unknown'],
-        'mouse': ['mouse'],
-        'rat': ['rat']
+        'mouse'     : ['m'],
+        'rat'       : ['r'],
+    }
+
+    surgeonlist = {
+
+        'EAB'   : ['lit'],
+        'KBH'   : ['kbh'],
+        'LIT'   : ['lit'],
+        'SCF'   : ['scf'],
+        'SJB'   : ['sjb'],
     }
 
     sexlist = {
         'unknown' : ['unknown'],
-        'female': ['female'],
-        'male'  : ['male'],
+        'female': ['f'],
+        'male'  : ['m'],
 
     }
 
     regionlist = {
 
-        'Unknown'   : ['unknown'],
-        'Basal Forebrain' : ['basalforebrain'],
-        'HC'    : ['hc'],
-        'NAc'   : ['nac'],
-        'RSC'   : ['rsc'],
-        'SCN'   : ['scn'],
-        'S1'    : ['s1'],
-        'Subiculum' : ['subiculum'],
-        'V1m'   : ['v1m'],
-        'V1b'   : ['v1b'],
-        'V2'    : ['v2'],
+        'Unknown'           : ['unknown'],
+        'Basal Forebrain'   : ['basalforebrain'],
+        'CA1'               : ['ca1'],
+        'DG'                : ['dg'],
+        'Endo ctx'          : ['endoctx'],
+        'Ento ctx'          : ['entoctx'],
+        'HC'                : ['hc'],
+        'LGN'               : ['lgn'],
+        'M1'                : ['m1'],
+        'm2'                : ['m2'],
+        'perirh ctx'        : ['perirh ctx'],
+        'NAc'               : ['nac'],
+        'RSC'               : ['rsc'],
+        'SCN'               : ['scn'],
+        'S1'                : ['s1'],
+        'Subiculum'         : ['subiculum'],
+        'V1m'               : ['v1m'],
+        'V1b'               : ['v1b'],
+        'V2'                : ['v2'],
 
     }
 
     # can conditionally display these based on species later on...
     strainlist = {
 
-        'Unknown'   : ['unknown'],
-        'Long Evans' : ['le'],
-        'C57 Black 6' : ['c57b6'],
+        'Unknown'       : ['unknown'],
+        'Long Evans'    : ['le'],
+        'C57 Black 6'   : ['c57b6'],
 
     }
 
@@ -158,8 +146,8 @@ def submit_implant():
     daqsyslist = {
 
         'Unknown'   : ['unknown'],
-        'ecube' : ['ecube'],
-        'intan' : ['intan']
+        'ecube'     : ['ecube'],
+        'intan'     : ['intan']
 
     }
 
@@ -172,19 +160,19 @@ def submit_implant():
 
     electrodelist = {
 
-        'Unknown'   : ['unknown'],
-        'tetrode' : ['tetrode'],
-        'stereotrode' : ['stereotrode'],
-        'single wire' : ['single'],
-        'carbon' : ['carbon'],
-        'MIT silicon' : ['mitsilicon'],
-        'UCLA silicon' : ['uclasilicon'],
-        'neuropixel' : ['neuropixel'],
+        'Unknown'       : ['unknown'],
+        'tetrode'       : ['tetrode'],
+        'stereotrode'   : ['stereotrode'],
+        'single wire'   : ['single'],
+        'carbon'        : ['carbon'],
+        'MIT silicon'   : ['mitsilicon'],
+        'UCLA silicon'  : ['uclasilicon'],
+        'neuropixel'    : ['neuropixel'],
     }
 
     headstagelist = {
 
-        'Unknown'   : ['unknown'],
+        'Unknown' : ['unknown'],
         'intan16' : ['intan16'],
         'intan32' : ['intan32'],
         'intan64' : ['intan64'],
@@ -192,11 +180,27 @@ def submit_implant():
 
     }
 
+    class PersonHandler(Handler):
+    """ Handler class to perform restructuring action when conditions are met.
+    The restructuring consists of replacing a ChildSpec instance by an
+    AdultSpec instance, or vice-versa. We would not need a handler to listen
+    for a change in age, but we do need a Handler so that Traits UI will
+    respond immediately to changes in the viewed Person, which require
+    immediate restructuring of the UI.
+    """
+
+        def object_age_changed(self, info):
+            if ((info.object.age >= 18) and
+                    (not isinstance(info.object.misc, AdultSpec))):
+                info.object.misc = AdultSpec()
+            elif ((info.object.age < 18) and
+                  (not isinstance(info.object.misc, ChildSpec))):
+                info.object.misc = ChildSpec()
+
     class implantupload(HasTraits):
         """
         """
         masterpath      = Directory(os.getcwd())
-        #topdir          = Button('Dir.') #demo1.configure_traits(),#Button( demo1.configure_traits() )# oh what to do...
         animalid        = Str ('ex. ABC12345')
         experiment_id   = Str ('ex. 0010101')
         species         = Enum(list(specieslist.keys())[0], list(specieslist.keys()))
@@ -251,54 +255,30 @@ def submit_implant():
             title = 'Implant Information.',
             buttons = ['OK'],
             resizable = True,
+            handler = PersonHandler()
 
         )
 
     # Create the demo:
-    demo = implantupload()
+    igui = implantupload()
 
     # Run the demo (if invoked from the command line):
     if __name__ == '__main__':
-        demo.configure_traits()
+        igui.configure_traits()
 
-    # # these are the fields that the user needs to manually enter prior to uploading anything into the database.
-    # # implant (primary key) # THIS IS THE PRIMARY KEY AND WILL AUTOMATICALLY POPULATE WITH A UNIQUE VALUE
-    # animal_id           = input ('Animal ID (initials plus 5 digits, ex: abc12345)  ')
-    # experiment_id       = input ('Experiment ID code (look up in lab file, or create)  ')
-    # species             = input ('Species (rat or mouse, r/m)  ')
-    # sex                 = input ('Sex (m or f)  ')
-    # region              = input ('Implant region (common lab abbreviation, e.g., v1, v1m, scn, rsc, s1)  ')
-    # strain              = input ('Animal Strain (ex. c57b6, le (long evans), sd (sprague dawley))  ')
-    # genotype            = input ('Animal genotype (ex. wt, shank3+-, shank3 --)  ')
-    # daqsys              = input ('Data Acq. System (ecube or intan)  ')
-    # nchan               = int( input ('Number of channels in this implant, e.g., 64  ') )
-    # chanrange           = input ('What is the channel range of this implant? (e.g. 65-128 as 65,128)  ')
-    # n_sites             = int( input ('Total number of implant sites in this animal  ') )
-    # implant_date        = input ('Implant date (MMDDYYYY)  ')
-    # expt_start          = input ('Experiment start (MMDDYYYY)  ')
-    # expt_end            = input ('Experiment end (MMDDYYYY)  ')
-    # age_t0              = int( input ('Age at start of experiment (postnatal day e.g., 60)  ') )
-    # surgeon             = input ('Surgeon (3 letter initial, e.g., abc)  ')
-    # video_binary        = int( input ('Video? (yes video = 1, no video = 0)  ') )
-    # light_binary        = int( input ('L/D timestamps? (1 or 0)  ') )
-    # sound_binary        = int( input ('Microphone data? (1 or 0)  ') )
-    # sw_binary           = int( input ('Sleep scoring? (1 or 0)  ') )
-    # implant_coordinates = input ('Stereotaxic coordinates (lateral, then bregma), e.g., -1.0, 2.2  ')
-    # electrode_type      = input ('Electrode type e.g., mit silicon, carbon, tetrode  ')
-    # headstage           = input ('Headstage e.g., hs640, hs64, intan32, intan64  ')
-    #
-    # # write in a check for this. if the user wants to, s/he should be able to go back and edit before committing the info to the database.
-    #
-    #
-    # # upload this into the database:
-    #  - - - - - - - - - - - - add data to a table - - - - - - - - - - - - -
-    ## defining the Query
+    return igui
+            # NEED TO GET THE INFORMATION OUT OF THE GUI OUTPUT AND PUT INTO DICTIONARY
 
-    # make sure that your target and value pairing match up with the fields that exist in the database.
-    # the "target" in quotes is the field name in the database. the value (not in quotes) is the name
-    # of the variable you're passing to the database.
-    
-        # NEED TO GET THE INFORMATION OUT OF THE GUI OUTPUT AND PUT INTO DICTIONARY
+def submit_implant():
+    import os
+    import os.path as op
+    import shutil
+    import csv
+    import pandas as pd
+    #from __future__ import absolute_import
+    import numpy as np
+
+    g = __implantgui()
 
     target_val_pair = {
             "animal_id": animal_id,
@@ -383,77 +363,76 @@ def clustercrawl(topdir,ucode,chans):
         under development.
         kbh 4/11/19
         '''
-        import numpy as np
-        import glob
-        import os
-        #cwd = os.getcwd() # save the current folder so that you can return the
-        #user to this as soon as the code finishes.
+    import numpy as np
+    import glob
+    import os
+    #cwd = os.getcwd() # save the current folder so that you can return the
+    #user to this as soon as the code finishes.
 
-        # figure out how many blocks (days?) of recording you're dealing with:
-        blocks      = glob.glob(topdir +'/'+ '*_unique_clusters.npy')
-        # extract the block's file name key
-        blocks  = [blocks[i][0 : blocks[i].find('unique_clusters.npy')] for i in np.arange(0,np.shape(blocks)[0])]
-        # extract the label component that is unique to this block and common to
-        # all components related to this block
-        #blocklabel  = blocks[0][0:blocks[0].find('unique_clusters.npy')])
+    # figure out how many blocks (days?) of recording you're dealing with:
+    blocks      = glob.glob(topdir +'/'+ '*_unique_clusters.npy')
+    # extract the block's file name key
+    blocks  = [blocks[i][0 : blocks[i].find('unique_clusters.npy')] for i in np.arange(0,np.shape(blocks)[0])]
+    # extract the label component that is unique to this block and common to
+    # all components related to this block
+    #blocklabel  = blocks[0][0:blocks[0].find('unique_clusters.npy')])
 
-        # set the directory
-        folder_location = topdir
+    # set the directory
+    folder_location = topdir
 
-        for block in blocks:
+    for block in blocks:
+        print(block)
+        #DO THE thing
 
-            #DO THE thing
+def cluststats(blocklabel):
+    # extract the clustering algorithm
+    clustal = np.load(blocklabel + 'algorithm.npy')
+    # figure out t0 for the block in YYMMDD_HHMMSS
+    unders = [i for i, letter in enumerate(blocklabel) if letter == '_']
+    t0 = blocklabel[unders[1]+1:unders[3]]
+    # - - - - - - - - - caluculate block duration  - - - - - - - -
+    # get the sample rate.
+    samplerate = np.squeeze( np.load( blocklabel + 'sampling_rate.npy') )
+    # read out the number of sample points in the block from the file name
+    ltext   = blocks[0].find('length_') + 7
+    dur     = np.int( blocks[0][ ltext : blocks[0].find( '_', ltext ) ] )
+    # convert sample points to time in seconds. might be better to store
+    # sample points and sample rate so nothing gets lost in rounding.
+    dur     /= samplerate
+    # Load the block idx. Extract cluster labels and counts
+    block_idx       = np.load(blocklabel + 'spike_clusters.npy')
+    unique, counts  = np.unique(block_idx, return_counts=True)
+    # get cluster quality. The default from KS2 is frequently junk
+    qs = np.load( blocklabel + 'basicclusterqual.npy' )
+    qs = qs[unique]
 
-        def cluststats(blocklabel):
+    # calculate firing rates.
+    clust_frs = counts/dur
+    # load amplitudes of all spikes
+    amps = np.load(blocklabel + 'amplitudes.npy')
+    # use list comprehension to calculate the mean amplitude by idx
+    mean_amps = [np.mean ( amps[block_idx == i] ) for i in unique  ]
 
-            # extract the clustering algorithm
-            clustal = np.load(blocklabel + 'algorithm.npy')
-            # figure out t0 for the block in YYMMDD_HHMMSS
-            unders = [i for i, letter in enumerate(blocklabel) if letter == '_']
-            t0 = blocklabel[unders[1]+1:unders[3]]
-            # - - - - - - - - - caluculate block duration  - - - - - - - -
-            # get the sample rate.
-            samplerate = np.squeeze( np.load( blocklabel + 'sampling_rate.npy') )
-            # read out the number of sample points in the block from the file name
-            ltext   = blocks[0].find('length_') + 7
-            dur     = np.int( blocks[0][ ltext : blocks[0].find( '_', ltext ) ] )
-            # convert sample points to time in seconds. might be better to store
-            # sample points and sample rate so nothing gets lost in rounding.
-            dur     /= samplerate
-            # Load the block idx. Extract cluster labels and counts
-            block_idx       = np.load(blocklabel + 'spike_clusters.npy')
-            unique, counts  = np.unique(block_idx, return_counts=True)
-            # get cluster quality. The default from KS2 is frequently junk
-            qs = np.load( blocklabel + 'basicclusterqual.npy' )
-            qs = qs[unique]
+    # - - - get waveform information - - - - - - - - -
+    wfs = np.load(blocklabel + 'template_waveform.npy')
 
-            # calculate firing rates.
-            clust_frs = counts/dur
-            # load amplitudes of all spikes
-            amps = np.load(blocklabel + 'amplitudes.npy')
-            # use list comprehension to calculate the mean amplitude by idx
-            mean_amps = [np.mean ( amps[block_idx == i] ) for i in unique  ]
-
-            # - - - get waveform information - - - - - - - - -
-            wfs = np.load(blocklabel + 'template_waveform.npy')
-
-            negpos  = np.zeros(unique.size)
-            halfs   = np.zeros(unique.size)
-            falling = np.zeros(unique.size)
-            count   = 0
-            for i in unique:
-                # calculate peak min to max time
-                negpos[count]   =  (wfs[i][wfs[i].argmin():-1].argmax()) * (1/samplerate)
-                # calculate the spike halfwidth
-                halfs[count]    = np.sum( wfs[i] < 0.5 * wfs[i].min() ) * (1/samplerate)
-                # calculate falling phase slope (appears rising on the extracellular WF)
-                # this is calculated as microvolts per millisecond
-                falling[count]  = (wfs[i][28] - wfs[i][24])/ (4*(1/samplerate)*1000)
+    negpos  = np.zeros(unique.size)
+    halfs   = np.zeros(unique.size)
+    falling = np.zeros(unique.size)
+    count   = 0
+    for i in unique:
+        # calculate peak min to max time
+        negpos[count]   =  (wfs[i][wfs[i].argmin():-1].argmax()) * (1/samplerate)
+        # calculate the spike halfwidth
+        halfs[count]    = np.sum( wfs[i] < 0.5 * wfs[i].min() ) * (1/samplerate)
+        # calculate falling phase slope (appears rising on the extracellular WF)
+        # this is calculated as microvolts per millisecond
+        falling[count]  = (wfs[i][28] - wfs[i][24])/ (4*(1/samplerate)*1000)
 
 
-                count +=1
+        count +=1
 
-            #WRITE THE THINGS YOU NEED TO A PANDAS DATAFRAME AND RETURN THAT
+    #WRITE THE THINGS YOU NEED TO A PANDAS DATAFRAME AND RETURN THAT
 
-        #convert chans from comma separated string to integers
-        chans = [int(i) for i in chans.split(',')]
+    #convert chans from comma separated string to integers
+    chans = [int(i) for i in chans.split(',')]
