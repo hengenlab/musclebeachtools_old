26b07f0f458307ad565be23569f6c0c98902d87f
Sahara Ensley <sahara.ensley@wustl.edu>
2019-07-19 13:33:11 -0500

    function to load clusters by phy quality

--- load_clusters.py
+++ load_clusters.py
@@ -14,14 +14,16 @@ def load_clusters(datadir, filt, rawdatadir=False, multi_probe=False, probeNumbe
     :param end_block: last block
     :return: list of cells
     """
-    fileList = mbt.makeFileList(datadir, rawdatadir, multi_probe, start_block, end_block, probeNumber)
+    fileList = mb.makeFileList(datadir, rawdatadir, multi_probe, start_block, end_block, probeNumber)
     unique_clusters = np.unique(fileList[0])
     try:
         qual_list = np.load('scrubbed_quality_' + str(start_block) + '.npy')
     except FileNotFoundError:
         qual_list = fileList[5]
+    if len(np.where(np.isnan(qual_list))[0])>0:
+        qual_list = fileList[5]
     neurons = []
-    for clust_idx in unique_clusters:
-        if qual_list[clust_idx] in filt:
-            neurons.append(mbt.neuron(datadir,rawdatadir, clust_idx=clust_idx,start_block = start_block, end_block = end_block, file_list=fileList))
+    for index, clust_idx in enumerate(unique_clusters):
+        if qual_list[clust_idx] in filt: # need to make this run through selected qual array, so index needs to be different
+            neurons.append(mb.neuron(datadir,rawdatadir, clust_idx=clust_idx,start_block = start_block, end_block = end_block, file_list=fileList))
     return neurons
