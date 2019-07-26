8eb42eb61984b3467588f97f87ce36eacbae18d1
Sahara Ensley <sahara.ensley@wustl.edu>
2019-07-26 16:17:56 -0500

    fixed load clusters

--- load_clusters.py
+++ load_clusters.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 
-def load_clusters(datadir, filt, file_startclust = False,rawdatadir=False, multi_probe=False, probeNumber=1, start_block=0, end_block=1):
+def load_clusters(datadir, filt, file_startclust = False,rawdatadir=False, multi_probe=False, probeNumber=1, start_block=0, end_block=1, scrubbed = False):
     """
 
     :param datadir: directory with KS2 output (t folder)
@@ -16,12 +16,18 @@ def load_clusters(datadir, filt, file_startclust = False,rawdatadir=False, multi
     """
     fileList = mbt.makeFileList(datadir, file_startclust, rawdatadir, multi_probe, start_block, end_block, probeNumber)
     unique_clusters = np.unique(fileList[0])
-    try:
-        qual_list = np.load('scrubbed_quality_' + str(start_block) + '.npy')
-    except FileNotFoundError:
-        qual_list = fileList[5]
     neurons = []
-    for i, clust_idx in enumerate(unique_clusters):
-        if qual_list[i] in filt:
-            neurons.append(mbt.neuron(datadir,rawdatadir, clust_idx=clust_idx,start_block = start_block, end_block = end_block, file_list=fileList))
-    return neurons
+    if scrubbed:
+        qual_list = np.load('scrubbed_quality_' + str(start_block) + '.npy')
+        print(f'Here is the quality list (scrubbed): {qual_list}')
+        for i, clust_idx in enumerate(unique_clusters):
+            if qual_list[i] in filt:
+                neurons.append(mbt.neuron(datadir, rawdatadir, clust_idx = clust_idx, start_block = start_block, end_block = end_block, file_list = fileList))
+        return neurons
+    else:
+        qual_list = fileList[5]
+        print(f'Here is the quality list: {qual_list}')
+        for i, clust_idx in enumerate(unique_clusters):
+            if qual_list[clust_idx] in filt:
+                neurons.append(mbt.neuron(datadir, rawdatadir, clust_idx = clust_idx, start_block = start_block, end_block = end_block, file_list = fileList))
+        return neurons
