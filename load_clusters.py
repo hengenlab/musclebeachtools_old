d4a5830ea45cc782b794594918b952c8991b875f
Sahara Ensley <sahara.ensley@wustl.edu>
2019-08-12 16:15:42 -0500

    small changes in load cluster to accomadate sleepwake states

--- load_clusters.py
+++ load_clusters.py
@@ -5,7 +5,7 @@ import glob
 
 
 
-def load_clusters(datadir, filt, tracked = False, file_startclust = False,rawdatadir=False, multi_probe=False, probeNumber=1, start_block=0, end_block=1, scrubbed = False):
+def load_clusters(datadir, filt, start_block, end_block, tracked = False, file_startclust = False,rawdatadir=False, multi_probe=False, probeNumber=1, scrubbed = False):
     """
 
     :param datadir: directory with KS2 output (t folder)
@@ -18,14 +18,13 @@ def load_clusters(datadir, filt, tracked = False, file_startclust = False,rawdat
     """
     neurons = []
     os.chdir(datadir)
-    fileList = mbt.makeFileList(datadir, file_startclust, rawdatadir, multi_probe, start_block, end_block, probeNumber)
+    fileList = mbt.makeFileList(datadir, file_startclust = file_startclust, rawdatadir=False, multi_probe = multi_probe, start_block = start_block, end_block = end_block, probeNumber = probeNumber)
     if tracked:
         keys = np.load(glob.glob('*keys*')[0])
         key_clusters = np.unique(np.where(keys != 0)[0])
 
         for i, clust_idx in enumerate(key_clusters): # this code picks the quality array to go off of for that cell, since every cell isn't on the first day you can't just go based on that
             if len(np.where(keys[clust_idx] == 0)) > 0:
-                continuous = False
                 first_block_present = np.where(keys[clust_idx] != 0)[0][0]
                 relative_block = first_block_present - start_block
                 if relative_block >= end_block:
